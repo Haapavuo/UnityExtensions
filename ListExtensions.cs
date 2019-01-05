0a14bd563f673a81013427c4952c6886245b393e
Jeri Haapavuo <jeri.haapavuo@gmail.com>
2019-01-05 16:09:39 +0200

    List<T>.AddTimes().

---
+++ ListExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+public static class ListExtensions
+{
+    public static void AddTimes<T>(this List<T> list, T element, int times)
+    {
+        for (int i = 0; i < times; i++)
+        {
+            list.Add(element);
+        }
+    }
+}
